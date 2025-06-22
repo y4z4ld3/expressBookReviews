ae7b5edc658c7330f82529d1c4be5dbcbccb4427
Yazalde Amaya <yazalde.amaya@gmail.com>
2025-06-22 13:37:05 -0400

    Tarea2y3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,14 +17,18 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    res.send(
+        Object.entries(books)
+        .filter(([id, libro]) => libro.author.toLowerCase() === author.toLowerCase())
+        .map(([id, libro]) => ({ id, ...libro }))
+    );
 });
 
 // Get all books based on title
